13e6b5723a1b8dc4e97ccf021b999a80c22fab4a
jpcmorais16 <jpcmorais2001@gmail.com>
2022-04-09 12:10:24 -0300

    lab04

---
+++ lab04/src/pt/c02oo/s03relacionamento/s04restaum/Peca.java
@@ -0,0 +1,53 @@
+package pt.c02oo.s03relacionamento.s04restaum;
+
+public class Peca {
+	Posicao posicao;
+	boolean viva = true;
+	public Peca(Posicao posicao) {
+		
+		this.posicao = posicao;
+		
+			
+	}
+	
+		
+	void come(Peca peca) {
+		
+		
+		if(peca.viva) {
+						
+			peca.morre();
+			
+		}	
+	}
+	
+	void move(Posicao nova) {
+		
+		this.desocupaPosicao();
+		this.posicao = nova;
+		
+		nova.conectaPeca(this);
+		
+		
+	}
+	
+	
+	
+	void morre() {
+		
+		viva = false;
+		desocupaPosicao();
+		
+	}
+	
+	
+	
+	void desocupaPosicao() {
+		
+		this.posicao.desocupa();
+		
+	}
+	
+	
+
+}
